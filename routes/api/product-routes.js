c318bcc0cf6d5d0ada7bffdf6dae10b04da84197
Natnael Haile <natnaelh14@github.com>
2021-07-26 17:09:31 -0400

    amended post routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -39,15 +39,14 @@ router.get('/:id', async(req, res) => {
 // create new product
 router.post('/', (req, res) => {
   
-  /* req.body should look like this...
+  Product.create(
     {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
+      product_name: req.body.product_name,
+      price: req.body.price,
+      stock: req.body.stock,
+      tagIds: req.body.tag_id
     }
-  */
-  Product.create(req.body)
+  )
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
